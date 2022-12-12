f3e7f12cad9ba1940d07e085af4b3b5ca7af099b
Christian Quispe <christianquispecamasca@gmail.com>
2022-12-11 23:39:18 -0500

    chore: config auth provider with context and reducer hooks

--- src/Auth/AuthProvider.tsx
+++ src/Auth/AuthProvider.tsx
@@ -1,52 +1,75 @@
 import React from "react";
+import { login, LoginParamsDto } from "./aplication/login";
+import { useNavigate } from "react-router-dom";
 
-const fakeAuthProvider = {
-  isAuthenticated: false,
-  signin(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = true;
-    setTimeout(callback, 100); // fake async
-  },
-  signout(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = false;
-    setTimeout(callback, 100);
-  },
-};
-
-interface AuthContextType {
-  user: any;
-  signin: (user: string, callback: VoidFunction) => void;
-  signout: (callback: VoidFunction) => void;
+export enum AuthActions {
+  signIn = "LOGIN",
+  signOut = "SIGN_OUT",
 }
 
-const AuthContext = React.createContext<AuthContextType>(null!);
+type AuthAction = { type: AuthActions, payload?: { token?: string, user?: any } };
+
+type Dispatch = React.Dispatch<AuthAction>;
+interface AuthContextState {
+  token?: string;
+  user?: any;
+  // signin: (user: string, callback: VoidFunction) => void;
+  // signout: (callback: VoidFunction) => void;
+}
+
+const AuthContext = React.createContext<
+  | {
+      state: AuthContextState;
+      dispatch: Dispatch;
+    }
+  | undefined
+>(undefined);
+
+function authReducer(state: AuthContextState, action: AuthAction): AuthContextState {
+  switch (action.type) {
+    case AuthActions.signIn: {
+      if (action.payload === undefined) {
+        throw new Error("signIn action must used with login params");
+      }
+      return { ...state, token: action.payload.token, user: action.payload.user };
+    }
+    case AuthActions.signOut: {
+      return { ...state, token: undefined, user: undefined };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
 
 export default function AuthProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [user, setUser] = React.useState<any>(null);
-  const [token, setToken] = React.useState<string | null>(null);
+  const [state, dispatch] = React.useReducer(authReducer, {});
 
-  const signin = (newUser: string, callback: VoidFunction) => {
-    return fakeAuthProvider.signin(() => {
-      setUser(newUser);
-      callback();
-    });
-  };
-
-  const signout = (callback: VoidFunction) => {
-    return fakeAuthProvider.signout(() => {
-      setUser(null);
-      callback();
-    });
-  };
-
-  const value = { user, signin, signout };
+  const value = { state, dispatch };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export function useAuth() {
-  return React.useContext(AuthContext);
+  const context = React.useContext(AuthContext);
+  const navigate = useNavigate();
+
+  if (context === undefined) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+
+  const signin = async (params: LoginParamsDto) => {
+    const { token, user } = await login(params);
+    context.dispatch({ type: AuthActions.signIn, payload: { token, user } });
+  };
+
+  const signout = (callback?: VoidFunction) => {
+    context.dispatch({ type: AuthActions.signOut });
+    navigate("/");
+  };
+  return { ...context, isLogged: Boolean(context.state.token), signin, signout };
 }
